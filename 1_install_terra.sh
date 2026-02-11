ffa8508f5b23e64752f7b095c124c6d655d29c12
Manish Wagh <167597076+Mwagh5022Git@users.noreply.github.com>
2026-02-11 18:14:43 +0530

    Update 1_install_terra.sh

--- 1_install_terra.sh
+++ 1_install_terra.sh
@@ -13,39 +13,8 @@ sudo apt update && sudo apt install terraform
 else
   echo "Terraform already installed, skipping..."
 
-
 fi
 
 terraform version
 mkdir terra_project_2
 cd terra_project_2
-
-# Create Terraform File with content
-cat <<EOF > main.tf
-resource "random_string" "first" {
-  length = 20
-}
-EOF
-echo "main.tf created."
-
-echo "Running terraform init..."
-terraform init
-
-echo "Usage:"
-echo "  ./main.sh install   # Install Terraform (run once)"
-echo "  ./main.sh setup     # Create main.tf"
-echo "  ./main.sh init      # Terraform init"
-echo "  ./main.sh apply     # Terraform apply"
-# exit 1
-
-terraform plan
-
-# Backup 
-terraform plan -out myplan
-
-echo "Running terraform apply..."
-terraform apply myplan
-
-
-#   chmod 755 main.sh
-#   ./main.sh
